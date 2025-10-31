a0ac964f64122622950deb2379d6d0dc288090cc
wung-wung <3047605407@qq.com>
2025-10-31 18:32:32 +0800

    Site updated: 2025-10-31 18:32:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/31/hello-world/"];function toRandomPost(){
+var posts=["2025/10/29/我的网站搭建历程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
