c84ed48614c4bcb336a69003c5403407b24530ab
wung-wung <3047605407@qq.com>
2025-11-01 11:11:21 +0800

    Site updated: 2025-11-01 11:11:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/29/我的网站搭建历程/","2025/10/31/数据结构·线性表/","2025/10/31/鸿蒙学习难点~数据扁平化/"];function toRandomPost(){
+var posts=["2025/10/31/数据结构·线性表/","2025/10/29/我的网站搭建历程/","2025/10/31/鸿蒙学习难点~数据扁平化/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
